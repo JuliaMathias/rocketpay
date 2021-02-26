c8e6a93c61bfc12f916366df6fe03c24d98d6ab2
Julia Mathias <juliamss@gmail.com>
2021-02-25 22:00:08 -0300

    Add transaction function to accounts controller

--- lib/rocketpay_web/controllers/accounts_controller.ex
+++ lib/rocketpay_web/controllers/accounts_controller.ex
@@ -23,11 +23,11 @@ defmodule RocketpayWeb.AccountsController do
     end
   end
 
-  # def transaction(conn, params) do
-  #   with {:ok, %TransactionResponse{} = transaction} <- Rocketpay.transaction(params) do
-  #     conn
-  #     |> put_status(:ok)
-  #     |> render("transaction.json", transaction: transaction)
-  #   end
-  # end
+  def transaction(conn, params) do
+    with {:ok, %TransactionResponse{} = transaction} <- Rocketpay.transaction(params) do
+      conn
+      |> put_status(:ok)
+      |> render("transaction.json", transaction: transaction)
+    end
+  end
 end
